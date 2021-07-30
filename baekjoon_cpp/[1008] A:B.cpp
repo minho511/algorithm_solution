1a40e93bd4e6afe7a47e0cf3919eedc84e41ae4a
minho511 <m980107@naver.com>
2021-07-30 14:30:58 +0900

    up

---
+++ baekjoon_cpp/[1008] A:B.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void){
+    std::cout.precision(10);
+    double a;
+    double b;
+    std::cin>>a;
+    std::cin>>b;
+    std::cout<<a/b<<std::endl;
+    return 0;
+}
