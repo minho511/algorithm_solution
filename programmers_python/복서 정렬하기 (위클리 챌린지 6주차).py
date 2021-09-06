41bacde2b1ef3d1cde3459a5a6649be5b0d4831c
minho511 <m980107@naver.com>
2021-09-06 16:38:21 +0900

    upload 0906

---
+++ programmers_python/복서 정렬하기 (위클리 챌린지 6주차).py
@@ -0,0 +1,36 @@
+# 프로그래머스 위클리챌린지 6주차
+# https://programmers.co.kr/learn/courses/30/lessons/85002
+def solution(weights, head2head):
+    n = len(weights)
+    count = [0]*n  # 자신보다 몸무게가 무거운 복서를 이긴 횟수
+    win = [0]*n  # 이긴 횟수
+    fight = [0]*n  # 각 선수별 경기수
+    answer = []
+    for i in range(n):
+        for j in range(i+1, n):
+            if head2head[i][j] == 'W':
+                fight[i] +=1
+                fight[j] +=1
+                win[i] += 1
+                if weights[i] < weights[j]:
+                    count[i]+=1
+            elif head2head[i][j] == 'L':
+                fight[i] +=1
+                fight[j] +=1
+                win[j] +=1
+                if weights[j] < weights[i]:
+                    count[j]+=1
+    per = [0]*n  # 승률
+    data = []
+    for i in range(n):
+        if fight[i] != 0:
+            per[i] = win[i]/fight[i]
+        data.append([-per[i], -count[i], -weights[i], i+1])
+    data.sort()
+    for i in range(n):
+        answer.append(data[i][-1])
+    return answer
+
+
+print(solution([50,82,75,120],["NLWL","WNLL","LWNW","WWLN"]))
+# result = [3,4,1,2]
