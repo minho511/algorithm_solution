297eda1af84ca886c787346d0bde87d1294a82b4
minho511 <mhkim98@seoultech.ac.kr>
2024-11-02 23:47:52 +0900

    20241102

--- baekjoon_python/[30508] 고인물이싫어.py
+++ baekjoon_python/[30508] 고인물이싫어.py
@@ -16,11 +16,12 @@ for i in range(n):
 k = int(input())
 
 visit = [[0]*m for _ in range(n)]
+sum_visit = [[0]*(m+1) for _ in range(n+1)]
 
 for _ in range(k):
     cx, cy = map(int, input().split())
     q.append((cx-1, cy-1))
-    
+
 while q:
     cx, cy = q.popleft()
     if visit[cx][cy]: continue
@@ -30,14 +31,18 @@ while q:
         if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] >= graph[cx][cy] and not visit[nx][ny]: 
             q.append((nx, ny))
 
-def check(i, j):
-    for p in range(i, i+h):
-        for q in range(j, j+w):
-            if not visit[p][q]: return 0
-    return 1
+for i in range(n):
+    for j in range(m):
+        sum_visit[i+1][j+1] = visit[i][j] + sum_visit[i+1][j] + sum_visit[i][j+1] - sum_visit[i][j]
 
-for i in range(n-h+1):
-    for j in range(m-w+1):
-        cnt += check(i, j)
+def check(x, y, h, w):
+    area_sum = sum_visit[x+h][y+w] - sum_visit[x][y+w] - sum_visit[x+h][y] + sum_visit[x][y]
+    return area_sum == h * w
+
+
+for i in range(n - h + 1):
+    for j in range(m - w + 1):
+        if check(i, j, h, w):
+            cnt += 1
 
 print(cnt)
