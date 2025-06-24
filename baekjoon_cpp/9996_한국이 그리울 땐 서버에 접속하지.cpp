e8975cbf7f8465cc239a01ab3c6314c18fb55591
minho511 <mhkim98@seoultech.ac.kr>
2025-06-24 17:46:55 +0900

    20250624

--- baekjoon_cpp/9996_한국이 그리울 땐 서버에 접속하지.cpp
+++ baekjoon_cpp/9996_한국이 그리울 땐 서버에 접속하지.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//mysol
 int n;
 int i, j, k;
 string s, s2;
@@ -45,3 +46,30 @@ int main(){
     }
     return 0;
 }
+
+
+// other example
+
+// int n;
+// string s, ori_s, pre, suf;
+
+// int main(){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+    
+//     cin >> n;
+//     cin >> ori_s;
+//     int pos = ori_s.find('*');
+//     pre = ori_s.substr(0, pos);
+//     suf = ori_s.substr(pos +1);
+//     for(int t = 0; t<n; t++){
+//         cin >> s;
+//         if(pre.size()+suf.size() > s.size()) cout << "NE\n";
+//         else{
+//             if(pre == s.substr(0, pre.size()) && suf == s.substr(s.size()-suf.size())) cout << "DA\n";
+//             else cout << "NE\n";
+//         }
+//     }
+//     return 0;
+// }
