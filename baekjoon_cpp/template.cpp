c7bc09a601f2d2069ea4d0204cbb1cddbbdb2d7e
minho511 <mhkim98@seoultech.ac.kr>
2025-07-15 17:45:50 +0900

    20250715

--- baekjoon_cpp/template.cpp
+++ baekjoon_cpp/template.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 typedef long long ll;
 const int MaxN = 987654321;
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-        
+    int n, m;
+    cin >> n >> m;
+
+    
+
     return 0;
 }
